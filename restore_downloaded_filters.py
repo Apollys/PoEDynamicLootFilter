2e8a2b2b648fda126c5d1b707d82aed3959bbf15
Apollys <caheiner@gmail.com>
2021-08-22 23:54:54 -0700

    Update restore_downloaded_filters.py to actually restore *all* downloaded filters

--- restore_downloaded_filters.py
+++ restore_downloaded_filters.py
@@ -1,9 +1,16 @@
+import os
+import os.path
 import shutil
 
 import config
 
 def main():
-    shutil.copyfile(config.kInputLootFilterFullpath, config.kDownloadedLootFilterFullpath)
+    for filename in os.listdir(config.kInputLootFilterDirectory):
+        if (filename.endswith('.filter')):
+            source_fullpath = os.path.join(config.kInputLootFilterDirectory, filename)
+            destination_fullpath = os.path.join(config.kDownloadDirectory, filename)
+            shutil.copyfile(source_fullpath, destination_fullpath)
+            print('Restored {}'.format(destination_fullpath))
 
 if __name__ == '__main__':
     main()
